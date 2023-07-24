1015eebcf75b91f3cbca5b1c1a68c35437cf9cfb
karabodot <140389169+karabodot@users.noreply.github.com>
2023-07-24 21:00:13 +0200

    Add files via upload

---
+++ MySecondFrame.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tut.ac.za.ui;
+
+import java.awt.Color;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author Karabo Dotwana
+ */
+public class MySecondFrame extends JFrame{
+    public MySecondFrame(){
+        
+        setTitle("Karabo Dotwana");
+        setSize(200,250);
+        setDefaultLookAndFeelDecorated(true);
+        setResizable(true);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setForeground(Color.RED);
+        setVisible(true);
+    }
+    
+}
